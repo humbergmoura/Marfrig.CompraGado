034d8578a51b717cfe833e7bb61bab337a78f8c2
DadosEstudantes <56877756+humbergmoura@users.noreply.github.com>
2022-09-08 15:15:17 -0300

    Ajustes de campo preço.

--- UI/frmAnimal.cs
+++ UI/frmAnimal.cs
@@ -195,7 +195,7 @@ public partial class frmAnimal : Form
 
     private void txtPreco_KeyPress(object sender, KeyPressEventArgs e)
     {
-        if (!char.IsControl(e.KeyChar) || !char.IsDigit(e.KeyChar) || (e.KeyChar != ','))
+        if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != ','))
         {
             e.Handled = true;
         }
